3157338a56712133b841269a72999764d187986e
Axperty <kevingelhorn0@gmail.com>
2024-04-24 13:49:31 -0300

    Texture improvements to all drawers and cabinets.

--- src/main/java/com/axperty/storagedelight/StorageDelight.java
+++ src/main/java/com/axperty/storagedelight/StorageDelight.java
@@ -9,7 +9,6 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(StorageDelight.MOD_ID)
 public class StorageDelight {
     public static final String MOD_ID = "storagedelight";
