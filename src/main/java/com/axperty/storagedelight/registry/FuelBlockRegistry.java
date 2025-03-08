3dc32532a907097615cd5916a59247be870e07da
axperty <kevingelhorn0@gmail.com>
2025-03-08 16:56:05 -0300

    Added bookshelves with doors, see changelog.md for the latest changes.

--- src/main/java/com/axperty/storagedelight/registry/FuelBlockRegistry.java
+++ src/main/java/com/axperty/storagedelight/registry/FuelBlockRegistry.java
@@ -14,6 +14,7 @@ public class FuelBlockRegistry {
             registry.add(BlockRegistry.OAK_DRAWER, 300);
             registry.add(BlockRegistry.OAK_DRAWER_WITH_DOOR, 300);
             registry.add(BlockRegistry.OAK_DRAWER_WITH_BOOKS, 300);
+            registry.add(BlockRegistry.OAK_BOOKSHELF_WITH_DOOR, 300);
             registry.add(BlockRegistry.GLASS_OAK_CABINET, 300);
             registry.add(BlockRegistry.OAK_CABINET_WITH_GLASS_DOORS, 300);
             registry.add(BlockRegistry.OAK_SINGLE_DOOR_CABINET, 300);
@@ -22,6 +23,7 @@ public class FuelBlockRegistry {
             registry.add(BlockRegistry.BIRCH_DRAWER, 300);
             registry.add(BlockRegistry.BIRCH_DRAWER_WITH_DOOR, 300);
             registry.add(BlockRegistry.BIRCH_DRAWER_WITH_BOOKS, 300);
+            registry.add(BlockRegistry.BIRCH_BOOKSHELF_WITH_DOOR, 300);
             registry.add(BlockRegistry.GLASS_BIRCH_CABINET, 300);
             registry.add(BlockRegistry.BIRCH_CABINET_WITH_GLASS_DOORS, 300);
             registry.add(BlockRegistry.BIRCH_SINGLE_DOOR_CABINET, 300);
@@ -30,6 +32,7 @@ public class FuelBlockRegistry {
             registry.add(BlockRegistry.SPRUCE_DRAWER, 300);
             registry.add(BlockRegistry.SPRUCE_DRAWER_WITH_DOOR, 300);
             registry.add(BlockRegistry.SPRUCE_DRAWER_WITH_BOOKS, 300);
+            registry.add(BlockRegistry.SPRUCE_BOOKSHELF_WITH_DOOR, 300);
             registry.add(BlockRegistry.GLASS_SPRUCE_CABINET, 300);
             registry.add(BlockRegistry.SPRUCE_CABINET_WITH_GLASS_DOORS, 300);
             registry.add(BlockRegistry.SPRUCE_SINGLE_DOOR_CABINET, 300);
@@ -38,6 +41,7 @@ public class FuelBlockRegistry {
             registry.add(BlockRegistry.JUNGLE_DRAWER, 300);
             registry.add(BlockRegistry.JUNGLE_DRAWER_WITH_DOOR, 300);
             registry.add(BlockRegistry.JUNGLE_DRAWER_WITH_BOOKS, 300);
+            registry.add(BlockRegistry.JUNGLE_BOOKSHELF_WITH_DOOR, 300);
             registry.add(BlockRegistry.GLASS_JUNGLE_CABINET, 300);
             registry.add(BlockRegistry.JUNGLE_CABINET_WITH_GLASS_DOORS, 300);
             registry.add(BlockRegistry.JUNGLE_SINGLE_DOOR_CABINET, 300);
@@ -46,6 +50,7 @@ public class FuelBlockRegistry {
             registry.add(BlockRegistry.ACACIA_DRAWER, 300);
             registry.add(BlockRegistry.ACACIA_DRAWER_WITH_DOOR, 300);
             registry.add(BlockRegistry.ACACIA_DRAWER_WITH_BOOKS, 300);
+            registry.add(BlockRegistry.ACACIA_BOOKSHELF_WITH_DOOR, 300);
             registry.add(BlockRegistry.GLASS_ACACIA_CABINET, 300);
             registry.add(BlockRegistry.ACACIA_CABINET_WITH_GLASS_DOORS, 300);
             registry.add(BlockRegistry.ACACIA_SINGLE_DOOR_CABINET, 300);
@@ -54,6 +59,7 @@ public class FuelBlockRegistry {
             registry.add(BlockRegistry.DARK_OAK_DRAWER, 300);
             registry.add(BlockRegistry.DARK_OAK_DRAWER_WITH_DOOR, 300);
             registry.add(BlockRegistry.DARK_OAK_DRAWER_WITH_BOOKS, 300);
+            registry.add(BlockRegistry.DARK_OAK_BOOKSHELF_WITH_DOOR, 300);
             registry.add(BlockRegistry.GLASS_DARK_OAK_CABINET, 300);
             registry.add(BlockRegistry.DARK_OAK_CABINET_WITH_GLASS_DOORS, 300);
             registry.add(BlockRegistry.DARK_OAK_SINGLE_DOOR_CABINET, 300);
@@ -62,6 +68,7 @@ public class FuelBlockRegistry {
             registry.add(BlockRegistry.MANGROVE_DRAWER, 300);
             registry.add(BlockRegistry.MANGROVE_DRAWER_WITH_DOOR, 300);
             registry.add(BlockRegistry.MANGROVE_DRAWER_WITH_BOOKS, 300);
+            registry.add(BlockRegistry.MANGROVE_BOOKSHELF_WITH_DOOR, 300);
             registry.add(BlockRegistry.GLASS_MANGROVE_CABINET, 300);
             registry.add(BlockRegistry.MANGROVE_CABINET_WITH_GLASS_DOORS, 300);
             registry.add(BlockRegistry.MANGROVE_SINGLE_DOOR_CABINET, 300);
@@ -70,6 +77,7 @@ public class FuelBlockRegistry {
             registry.add(BlockRegistry.CHERRY_DRAWER, 300);
             registry.add(BlockRegistry.CHERRY_DRAWER_WITH_DOOR, 300);
             registry.add(BlockRegistry.CHERRY_DRAWER_WITH_BOOKS, 300);
+            registry.add(BlockRegistry.CHERRY_BOOKSHELF_WITH_DOOR, 300);
             registry.add(BlockRegistry.GLASS_CHERRY_CABINET, 300);
             registry.add(BlockRegistry.CHERRY_CABINET_WITH_GLASS_DOORS, 300);
             registry.add(BlockRegistry.CHERRY_SINGLE_DOOR_CABINET, 300);
@@ -78,6 +86,7 @@ public class FuelBlockRegistry {
             registry.add(BlockRegistry.BAMBOO_DRAWER, 300);
             registry.add(BlockRegistry.BAMBOO_DRAWER_WITH_DOOR, 300);
             registry.add(BlockRegistry.BAMBOO_DRAWER_WITH_BOOKS, 300);
+            registry.add(BlockRegistry.BAMBOO_BOOKSHELF_WITH_DOOR, 300);
             registry.add(BlockRegistry.GLASS_BAMBOO_CABINET, 300);
             registry.add(BlockRegistry.BAMBOO_CABINET_WITH_GLASS_DOORS, 300);
             registry.add(BlockRegistry.BAMBOO_SINGLE_DOOR_CABINET, 300);
