077037056b796d768abeab81fece5d97152a7474
axperty <kevingelhorn0@gmail.com>
2025-03-08 18:00:45 -0300

    Added bookshelves with doors, see changelog.md for the latest changes.

--- src/main/java/com/axperty/storagedelight/registry/BlockRegistry.java
+++ src/main/java/com/axperty/storagedelight/registry/BlockRegistry.java
@@ -6,6 +6,7 @@ import com.axperty.storagedelight.block.DrawerDoorBlock;
 import com.axperty.storagedelight.block.DrawerBooksBlock;
 import com.axperty.storagedelight.block.GlassCabinetBlock;
 import com.axperty.storagedelight.block.CabinetVariantBlock;
+import com.axperty.storagedelight.block.BookshelfDoorBlock;
 import net.fabricmc.fabric.api.registry.FlammableBlockRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.registry.Registries;
@@ -20,6 +21,7 @@ public enum BlockRegistry {
     OAK_DRAWER("oak_drawer", DrawerBlock::new),
     OAK_DRAWER_WITH_DOOR("oak_drawer_with_door", DrawerDoorBlock::new),
     OAK_DRAWER_WITH_BOOKS("oak_drawer_with_books", DrawerBooksBlock::new),
+    OAK_BOOKSHELF_WITH_DOOR("oak_bookshelf_with_door", BookshelfDoorBlock::new),
     GLASS_OAK_CABINET("glass_oak_cabinet", GlassCabinetBlock::new),
     OAK_CABINET_WITH_GLASS_DOORS("oak_cabinet_with_glass_doors", CabinetVariantBlock::new),
     OAK_SINGLE_DOOR_CABINET("oak_single_door_cabinet", CabinetVariantBlock::new),
@@ -28,6 +30,7 @@ public enum BlockRegistry {
     BIRCH_DRAWER("birch_drawer", DrawerBlock::new),
     BIRCH_DRAWER_WITH_DOOR("birch_drawer_with_door", DrawerDoorBlock::new),
     BIRCH_DRAWER_WITH_BOOKS("birch_drawer_with_books", DrawerBooksBlock::new),
+    BIRCH_BOOKSHELF_WITH_DOOR("birch_bookshelf_with_door", BookshelfDoorBlock::new),
     GLASS_BIRCH_CABINET("glass_birch_cabinet", GlassCabinetBlock::new),
     BIRCH_CABINET_WITH_GLASS_DOORS("birch_cabinet_with_glass_doors", CabinetVariantBlock::new),
     BIRCH_SINGLE_DOOR_CABINET("birch_single_door_cabinet", CabinetVariantBlock::new),
@@ -36,6 +39,7 @@ public enum BlockRegistry {
     SPRUCE_DRAWER("spruce_drawer", DrawerBlock::new),
     SPRUCE_DRAWER_WITH_DOOR("spruce_drawer_with_door", DrawerDoorBlock::new),
     SPRUCE_DRAWER_WITH_BOOKS("spruce_drawer_with_books", DrawerBooksBlock::new),
+    SPRUCE_BOOKSHELF_WITH_DOOR("spruce_bookshelf_with_door", BookshelfDoorBlock::new),
     GLASS_SPRUCE_CABINET("glass_spruce_cabinet", GlassCabinetBlock::new),
     SPRUCE_CABINET_WITH_GLASS_DOORS("spruce_cabinet_with_glass_doors", CabinetVariantBlock::new),
     SPRUCE_SINGLE_DOOR_CABINET("spruce_single_door_cabinet", CabinetVariantBlock::new),
@@ -44,6 +48,7 @@ public enum BlockRegistry {
     JUNGLE_DRAWER("jungle_drawer", DrawerBlock::new),
     JUNGLE_DRAWER_WITH_DOOR("jungle_drawer_with_door", DrawerDoorBlock::new),
     JUNGLE_DRAWER_WITH_BOOKS("jungle_drawer_with_books", DrawerBooksBlock::new),
+    JUNGLE_BOOKSHELF_WITH_DOOR("jungle_bookshelf_with_door", BookshelfDoorBlock::new),
     GLASS_JUNGLE_CABINET("glass_jungle_cabinet", GlassCabinetBlock::new),
     JUNGLE_CABINET_WITH_GLASS_DOORS("jungle_cabinet_with_glass_doors", CabinetVariantBlock::new),
     JUNGLE_SINGLE_DOOR_CABINET("jungle_single_door_cabinet", CabinetVariantBlock::new),
@@ -52,6 +57,7 @@ public enum BlockRegistry {
     ACACIA_DRAWER("acacia_drawer", DrawerBlock::new),
     ACACIA_DRAWER_WITH_DOOR("acacia_drawer_with_door", DrawerDoorBlock::new),
     ACACIA_DRAWER_WITH_BOOKS("acacia_drawer_with_books", DrawerBooksBlock::new),
+    ACACIA_BOOKSHELF_WITH_DOOR("acacia_bookshelf_with_door", BookshelfDoorBlock::new),
     GLASS_ACACIA_CABINET("glass_acacia_cabinet", GlassCabinetBlock::new),
     ACACIA_CABINET_WITH_GLASS_DOORS("acacia_cabinet_with_glass_doors", CabinetVariantBlock::new),
     ACACIA_SINGLE_DOOR_CABINET("acacia_single_door_cabinet", CabinetVariantBlock::new),
@@ -60,6 +66,7 @@ public enum BlockRegistry {
     DARK_OAK_DRAWER("dark_oak_drawer", DrawerBlock::new),
     DARK_OAK_DRAWER_WITH_DOOR("dark_oak_drawer_with_door", DrawerDoorBlock::new),
     DARK_OAK_DRAWER_WITH_BOOKS("dark_oak_drawer_with_books", DrawerBooksBlock::new),
+    DARK_OAK_BOOKSHELF_WITH_DOOR("dark_oak_bookshelf_with_door", BookshelfDoorBlock::new),
     GLASS_DARK_OAK_CABINET("glass_dark_oak_cabinet", GlassCabinetBlock::new),
     DARK_OAK_CABINET_WITH_GLASS_DOORS("dark_oak_cabinet_with_glass_doors", CabinetVariantBlock::new),
     DARK_OAK_SINGLE_DOOR_CABINET("dark_oak_single_door_cabinet", CabinetVariantBlock::new),
@@ -68,6 +75,7 @@ public enum BlockRegistry {
     MANGROVE_DRAWER("mangrove_drawer", DrawerBlock::new),
     MANGROVE_DRAWER_WITH_DOOR("mangrove_drawer_with_door", DrawerDoorBlock::new),
     MANGROVE_DRAWER_WITH_BOOKS("mangrove_drawer_with_books", DrawerBooksBlock::new),
+    MANGROVE_BOOKSHELF_WITH_DOOR("mangrove_bookshelf_with_door", BookshelfDoorBlock::new),
     GLASS_MANGROVE_CABINET("glass_mangrove_cabinet", GlassCabinetBlock::new),
     MANGROVE_CABINET_WITH_GLASS_DOORS("mangrove_cabinet_with_glass_doors", CabinetVariantBlock::new),
     MANGROVE_SINGLE_DOOR_CABINET("mangrove_single_door_cabinet", CabinetVariantBlock::new),
@@ -76,6 +84,7 @@ public enum BlockRegistry {
     CHERRY_DRAWER("cherry_drawer", DrawerBlock::new),
     CHERRY_DRAWER_WITH_DOOR("cherry_drawer_with_door", DrawerDoorBlock::new),
     CHERRY_DRAWER_WITH_BOOKS("cherry_drawer_with_books", DrawerBooksBlock::new),
+    CHERRY_BOOKSHELF_WITH_DOOR("cherry_bookshelf_with_door", BookshelfDoorBlock::new),
     GLASS_CHERRY_CABINET("glass_cherry_cabinet", GlassCabinetBlock::new),
     CHERRY_CABINET_WITH_GLASS_DOORS("cherry_cabinet_with_glass_doors", CabinetVariantBlock::new),
     CHERRY_SINGLE_DOOR_CABINET("cherry_single_door_cabinet", CabinetVariantBlock::new),
@@ -84,6 +93,7 @@ public enum BlockRegistry {
     BAMBOO_DRAWER("bamboo_drawer", DrawerBlock::new),
     BAMBOO_DRAWER_WITH_DOOR("bamboo_drawer_with_door", DrawerDoorBlock::new),
     BAMBOO_DRAWER_WITH_BOOKS("bamboo_drawer_with_books", DrawerBooksBlock::new),
+    BAMBOO_BOOKSHELF_WITH_DOOR("bamboo_bookshelf_with_door", BookshelfDoorBlock::new),
     GLASS_BAMBOO_CABINET("glass_bamboo_cabinet", GlassCabinetBlock::new),
     BAMBOO_CABINET_WITH_GLASS_DOORS("bamboo_cabinet_with_glass_doors", CabinetVariantBlock::new),
     BAMBOO_SINGLE_DOOR_CABINET("bamboo_single_door_cabinet", CabinetVariantBlock::new),
@@ -92,6 +102,7 @@ public enum BlockRegistry {
     CRIMSON_DRAWER("crimson_drawer", DrawerBlock::new),
     CRIMSON_DRAWER_WITH_DOOR("crimson_drawer_with_door", DrawerDoorBlock::new),
     CRIMSON_DRAWER_WITH_BOOKS("crimson_drawer_with_books", DrawerBooksBlock::new),
+    CRIMSON_BOOKSHELF_WITH_DOOR("crimson_bookshelf_with_door", BookshelfDoorBlock::new),
     GLASS_CRIMSON_CABINET("glass_crimson_cabinet", GlassCabinetBlock::new),
     CRIMSON_CABINET_WITH_GLASS_DOORS("crimson_cabinet_with_glass_doors", CabinetVariantBlock::new),
     CRIMSON_SINGLE_DOOR_CABINET("crimson_single_door_cabinet", CabinetVariantBlock::new),
@@ -100,6 +111,7 @@ public enum BlockRegistry {
     WARPED_DRAWER("warped_drawer", DrawerBlock::new),
     WARPED_DRAWER_WITH_DOOR("warped_drawer_with_door", DrawerDoorBlock::new),
     WARPED_DRAWER_WITH_BOOKS("warped_drawer_with_books", DrawerBooksBlock::new),
+    WARPED_BOOKSHELF_WITH_DOOR("warped_bookshelf_with_door", BookshelfDoorBlock::new),
     GLASS_WARPED_CABINET("glass_warped_cabinet", GlassCabinetBlock::new),
     WARPED_CABINET_WITH_GLASS_DOORS("warped_cabinet_with_glass_doors", CabinetVariantBlock::new),
     WARPED_SINGLE_DOOR_CABINET("warped_single_door_cabinet", CabinetVariantBlock::new);
