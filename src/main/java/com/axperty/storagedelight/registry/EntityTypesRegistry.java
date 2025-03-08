ebeb9df7699629262be1db2d31b78b0a18aeeb69
axperty <kevingelhorn0@gmail.com>
2025-03-08 17:30:15 -0300

    Added bookshelves with doors, see changelog.md for the latest changes.

--- src/main/java/com/axperty/storagedelight/registry/EntityTypesRegistry.java
+++ src/main/java/com/axperty/storagedelight/registry/EntityTypesRegistry.java
@@ -6,6 +6,7 @@ import com.axperty.storagedelight.block.entity.DrawerDoorBlockEntity;
 import com.axperty.storagedelight.block.entity.DrawerBooksBlockEntity;
 import com.axperty.storagedelight.block.entity.GlassCabinetBlockEntity;
 import com.axperty.storagedelight.block.entity.CabinetVariantBlockEntity;
+import com.axperty.storagedelight.block.entity.BookshelfDoorBlockEntity;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -99,5 +100,20 @@ public class EntityTypesRegistry {
                             BlockRegistry.MANGROVE_SINGLE_DOOR_CABINET.get(),
                             BlockRegistry.CRIMSON_SINGLE_DOOR_CABINET.get(),
                             BlockRegistry.WARPED_SINGLE_DOOR_CABINET.get())
+                    .build(null));
+
+    public static final Supplier<BlockEntityType<BookshelfDoorBlockEntity>> BOOKSHELF_DOOR = TILES.register("bookshelf_door",
+            () -> BlockEntityType.Builder.of(BookshelfDoorBlockEntity::new,
+                        BlockRegistry.OAK_BOOKSHELF_WITH_DOOR.get(),
+                        BlockRegistry.BIRCH_BOOKSHELF_WITH_DOOR.get(),
+                        BlockRegistry.SPRUCE_BOOKSHELF_WITH_DOOR.get(),
+                        BlockRegistry.JUNGLE_BOOKSHELF_WITH_DOOR.get(),
+                        BlockRegistry.ACACIA_BOOKSHELF_WITH_DOOR.get(),
+                        BlockRegistry.DARK_OAK_BOOKSHELF_WITH_DOOR.get(),
+                        BlockRegistry.BAMBOO_BOOKSHELF_WITH_DOOR.get(),
+                        BlockRegistry.CHERRY_BOOKSHELF_WITH_DOOR.get(),
+                        BlockRegistry.MANGROVE_BOOKSHELF_WITH_DOOR.get(),
+                        BlockRegistry.CRIMSON_BOOKSHELF_WITH_DOOR.get(),
+                        BlockRegistry.WARPED_BOOKSHELF_WITH_DOOR.get())
                     .build(null));
 }
