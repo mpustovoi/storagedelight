1725e0888e8741137cc5b1c1b235749ef4b09f5f
axperty <kevingelhorn0@gmail.com>
2025-03-09 00:17:51 -0300

    Added bookshelves with doors, see changelog.md for the latest changes.

--- src/main/java/com/axperty/storagedelight/registry/BlockEntityTypesRegistry.java
+++ src/main/java/com/axperty/storagedelight/registry/BlockEntityTypesRegistry.java
@@ -95,4 +95,19 @@ public class BlockEntityTypesRegistry {
                             BlockRegistry.CRIMSON_SINGLE_DOOR_CABINET.get(),
                             BlockRegistry.WARPED_SINGLE_DOOR_CABINET.get())
                     .build(null));
+
+    public static final RegistryObject<BlockEntityType<BookshelfDoorBlockEntity>> BOOKSHELF_DOOR = TILES.register("bookshelf_door",
+            () -> BlockEntityType.Builder.of(BookshelfDoorBlockEntity::new,
+                            BlockRegistry.OAK_BOOKSHELF_WITH_DOOR.get(),
+                            BlockRegistry.BIRCH_BOOKSHELF_WITH_DOOR.get(),
+                            BlockRegistry.SPRUCE_BOOKSHELF_WITH_DOOR.get(),
+                            BlockRegistry.JUNGLE_BOOKSHELF_WITH_DOOR.get(),
+                            BlockRegistry.ACACIA_BOOKSHELF_WITH_DOOR.get(),
+                            BlockRegistry.DARK_OAK_BOOKSHELF_WITH_DOOR.get(),
+                            BlockRegistry.BAMBOO_BOOKSHELF_WITH_DOOR.get(),
+                            BlockRegistry.CHERRY_BOOKSHELF_WITH_DOOR.get(),
+                            BlockRegistry.MANGROVE_BOOKSHELF_WITH_DOOR.get(),
+                            BlockRegistry.CRIMSON_BOOKSHELF_WITH_DOOR.get(),
+                            BlockRegistry.WARPED_BOOKSHELF_WITH_DOOR.get())
+                    .build(null));
 }
