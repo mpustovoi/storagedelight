787e98a32b0e229fcac10b71bf5c11fefee7c0df
axperty <kevingelhorn0@gmail.com>
2025-06-24 13:10:45 -0300

    Added support for Minecraft 1.21.6.

--- src/main/java/com/axperty/storagedelight/block/entity/DrawerBooksBlockEntity.java
+++ src/main/java/com/axperty/storagedelight/block/entity/DrawerBooksBlockEntity.java
@@ -2,17 +2,14 @@ package com.axperty.storagedelight.block.entity;
 
 import com.axperty.storagedelight.block.DrawerBooksBlock;
 import com.axperty.storagedelight.registry.EntityTypesRegistry;
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.HolderLookup;
-import net.minecraft.core.NonNullList;
-import net.minecraft.core.Vec3i;
-import net.minecraft.nbt.CompoundTag;
+import net.minecraft.core.*;
 import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.Container;
 import net.minecraft.world.ContainerHelper;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
@@ -22,112 +19,109 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.ContainerOpenersCounter;
 import net.minecraft.world.level.block.entity.RandomizableContainerBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.storage.ValueInput;
+import net.minecraft.world.level.storage.ValueOutput;
 
 public class DrawerBooksBlockEntity extends RandomizableContainerBlockEntity
 {
-    private NonNullList<ItemStack> contents = NonNullList.withSize(27, ItemStack.EMPTY);
-    private ContainerOpenersCounter openersCounter = new ContainerOpenersCounter()
-    {
-        protected void onOpen(Level level, BlockPos pos, BlockState state) {
-            DrawerBooksBlockEntity.this.playSound(state, SoundEvents.BARREL_OPEN);
-            DrawerBooksBlockEntity.this.updateBlockState(state, true);
-        }
+    private NonNullList<ItemStack> items;
+    private final ContainerOpenersCounter openersCounter;
 
-        protected void onClose(Level level, BlockPos pos, BlockState state) {
-            DrawerBooksBlockEntity.this.playSound(state, SoundEvents.BARREL_CLOSE);
-            DrawerBooksBlockEntity.this.updateBlockState(state, false);
-        }
-
-        protected void openerCountChanged(Level level, BlockPos pos, BlockState sta, int arg1, int arg2) {
-        }
-
-        protected boolean isOwnContainer(Player p_155060_) {
-            if (p_155060_.containerMenu instanceof ChestMenu) {
-                Container container = ((ChestMenu) p_155060_.containerMenu).getContainer();
-                return container == DrawerBooksBlockEntity.this;
-            } else {
-                return false;
+    public DrawerBooksBlockEntity(BlockPos pos, BlockState blockState) {
+        super(EntityTypesRegistry.DRAWER_BOOKS.get(), pos, blockState);
+        this.items = NonNullList.withSize(27, ItemStack.EMPTY);
+        this.openersCounter = new ContainerOpenersCounter() {
+            protected void onOpen(Level p_155062_, BlockPos p_155063_, BlockState p_155064_) {
+                DrawerBooksBlockEntity.this.playSound(p_155064_, SoundEvents.BARREL_OPEN);
+                DrawerBooksBlockEntity.this.updateBlockState(p_155064_, true);
             }
-        }
-    };
 
-    public DrawerBooksBlockEntity(BlockPos pos, BlockState state) {
-        super(EntityTypesRegistry.DRAWER_BOOKS.get(), pos, state);
+            protected void onClose(Level p_155072_, BlockPos p_155073_, BlockState p_155074_) {
+                DrawerBooksBlockEntity.this.playSound(p_155074_, SoundEvents.BARREL_CLOSE);
+                DrawerBooksBlockEntity.this.updateBlockState(p_155074_, false);
+            }
+
+            protected void openerCountChanged(Level p_155066_, BlockPos p_155067_, BlockState p_155068_, int p_155069_, int p_155070_) {
+            }
+
+            protected boolean isOwnContainer(Player p_155060_) {
+                if (p_155060_.containerMenu instanceof ChestMenu) {
+                    Container container = ((ChestMenu)p_155060_.containerMenu).getContainer();
+                    return container == DrawerBooksBlockEntity.this;
+                } else {
+                    return false;
+                }
+            }
+        };
     }
 
-    @Override
-    public void saveAdditional(CompoundTag compound, HolderLookup.Provider registries) {
-        super.saveAdditional(compound, registries);
-        if (!trySaveLootTable(compound)) {
-            ContainerHelper.saveAllItems(compound, contents, registries);
+    protected void saveAdditional(ValueOutput p_422559_) {
+        super.saveAdditional(p_422559_);
+        if (!this.trySaveLootTable(p_422559_)) {
+            ContainerHelper.saveAllItems(p_422559_, this.items);
         }
+
     }
 
-    @Override
-    public void loadAdditional(CompoundTag compound, HolderLookup.Provider registries) {
-        super.loadAdditional(compound, registries);
-        contents = NonNullList.withSize(getContainerSize(), ItemStack.EMPTY);
-        if (!tryLoadLootTable(compound)) {
-            ContainerHelper.loadAllItems(compound, contents, registries);
+    protected void loadAdditional(ValueInput p_422397_) {
+        super.loadAdditional(p_422397_);
+        this.items = NonNullList.withSize(this.getContainerSize(), ItemStack.EMPTY);
+        if (!this.tryLoadLootTable(p_422397_)) {
+            ContainerHelper.loadAllItems(p_422397_, this.items);
         }
+
     }
 
-    @Override
     public int getContainerSize() {
         return 27;
     }
 
-    @Override
     protected NonNullList<ItemStack> getItems() {
-        return contents;
+        return this.items;
     }
 
-    @Override
-    protected void setItems(NonNullList<ItemStack> itemsIn) {
-        contents = itemsIn;
+    protected void setItems(NonNullList<ItemStack> items) {
+        this.items = items;
     }
 
-    @Override
     protected Component getDefaultName() {
         return Component.translatable("container.storagedelight.drawer_books");
     }
 
-    @Override
     protected AbstractContainerMenu createMenu(int id, Inventory player) {
         return ChestMenu.threeRows(id, player, this);
     }
 
-    public void startOpen(Player pPlayer) {
-        if (level != null && !this.remove && !pPlayer.isSpectator()) {
-            this.openersCounter.incrementOpeners(pPlayer, level, this.getBlockPos(), this.getBlockState());
+    public void startOpen(Player player) {
+        if (!this.remove && !player.isSpectator()) {
+            this.openersCounter.incrementOpeners(player, this.getLevel(), this.getBlockPos(), this.getBlockState());
         }
+
     }
 
-    public void stopOpen(Player pPlayer) {
-        if (level != null && !this.remove && !pPlayer.isSpectator()) {
-            this.openersCounter.decrementOpeners(pPlayer, level, this.getBlockPos(), this.getBlockState());
+    public void stopOpen(Player player) {
+        if (!this.remove && !player.isSpectator()) {
+            this.openersCounter.decrementOpeners(player, this.getLevel(), this.getBlockPos(), this.getBlockState());
         }
+
     }
 
     public void recheckOpen() {
-        if (level != null && !this.remove) {
-            this.openersCounter.recheckOpeners(level, this.getBlockPos(), this.getBlockState());
+        if (!this.remove) {
+            this.openersCounter.recheckOpeners(this.getLevel(), this.getBlockPos(), this.getBlockState());
         }
+
     }
 
     void updateBlockState(BlockState state, boolean open) {
-        if (level != null) {
-            this.level.setBlock(this.getBlockPos(), state.setValue(DrawerBooksBlock.OPEN, open), 3);
-        }
+        this.level.setBlock(this.getBlockPos(), (BlockState)state.setValue(DrawerBooksBlock.OPEN, open), 3);
     }
 
-    private void playSound(BlockState state, SoundEvent sound) {
-        if (level == null) return;
-
-        Vec3i cabinetFacingVector = state.getValue(DrawerBooksBlock.FACING).getUnitVec3i();
-        double x = (double) worldPosition.getX() + 0.5D + (double) cabinetFacingVector.getX() / 2.0D;
-        double y = (double) worldPosition.getY() + 0.5D + (double) cabinetFacingVector.getY() / 2.0D;
-        double z = (double) worldPosition.getZ() + 0.5D + (double) cabinetFacingVector.getZ() / 2.0D;
-        level.playSound(null, x, y, z, sound, SoundSource.BLOCKS, 0.5F, level.random.nextFloat() * 0.1F + 0.9F);
+    void playSound(BlockState state, SoundEvent sound) {
+        Vec3i vec3i = ((Direction)state.getValue(DrawerBooksBlock.FACING)).getUnitVec3i();
+        double d0 = (double)this.worldPosition.getX() + (double)0.5F + (double)vec3i.getX() / (double)2.0F;
+        double d1 = (double)this.worldPosition.getY() + (double)0.5F + (double)vec3i.getY() / (double)2.0F;
+        double d2 = (double)this.worldPosition.getZ() + (double)0.5F + (double)vec3i.getZ() / (double)2.0F;
+        this.level.playSound((Entity)null, d0, d1, d2, sound, SoundSource.BLOCKS, 0.5F, this.level.random.nextFloat() * 0.1F + 0.9F);
     }
 }
