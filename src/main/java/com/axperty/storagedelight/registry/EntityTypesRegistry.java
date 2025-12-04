736f8ff1403bc6ba2b69aff20f7fee4debbcb23a
axperty <kevingelhorn0@gmail.com>
2025-12-03 21:08:32 -0300

    See changelog.md for the latest changes.

--- src/main/java/com/axperty/storagedelight/registry/EntityTypesRegistry.java
+++ src/main/java/com/axperty/storagedelight/registry/EntityTypesRegistry.java
@@ -127,4 +127,44 @@ public class EntityTypesRegistry {
                     BlockRegistry.SMALL_CRIMSON_DRAWERS.get(),
                     BlockRegistry.SMALL_WARPED_DRAWERS.get()
             )));
+
+    public static final RegistryObject<BlockEntityType<CabinetCountertopBlockEntity>> COUNTERTOP = TILES.register("countertop",
+            () -> new BlockEntityType<>(CabinetCountertopBlockEntity::new, Set.of(
+                    BlockRegistry.OAK_CABINET_WITH_POLISHED_DEEPSLATE_COUNTERTOP.get(),
+                    BlockRegistry.BIRCH_CABINET_WITH_POLISHED_DEEPSLATE_COUNTERTOP.get(),
+                    BlockRegistry.SPRUCE_CABINET_WITH_POLISHED_DEEPSLATE_COUNTERTOP.get(),
+                    BlockRegistry.JUNGLE_CABINET_WITH_POLISHED_DEEPSLATE_COUNTERTOP.get(),
+                    BlockRegistry.ACACIA_CABINET_WITH_POLISHED_DEEPSLATE_COUNTERTOP.get(),
+                    BlockRegistry.DARK_OAK_CABINET_WITH_POLISHED_DEEPSLATE_COUNTERTOP.get(),
+                    BlockRegistry.BAMBOO_CABINET_WITH_POLISHED_DEEPSLATE_COUNTERTOP.get(),
+                    BlockRegistry.CHERRY_CABINET_WITH_POLISHED_DEEPSLATE_COUNTERTOP.get(),
+                    BlockRegistry.MANGROVE_CABINET_WITH_POLISHED_DEEPSLATE_COUNTERTOP.get(),
+                    BlockRegistry.CRIMSON_CABINET_WITH_POLISHED_DEEPSLATE_COUNTERTOP.get(),
+                    BlockRegistry.WARPED_CABINET_WITH_POLISHED_DEEPSLATE_COUNTERTOP.get(),
+                    BlockRegistry.OAK_CABINET_WITH_POLISHED_ANDESITE_COUNTERTOP.get(),
+                    BlockRegistry.BIRCH_CABINET_WITH_POLISHED_ANDESITE_COUNTERTOP.get(),
+                    BlockRegistry.SPRUCE_CABINET_WITH_POLISHED_ANDESITE_COUNTERTOP.get(),
+                    BlockRegistry.JUNGLE_CABINET_WITH_POLISHED_ANDESITE_COUNTERTOP.get(),
+                    BlockRegistry.ACACIA_CABINET_WITH_POLISHED_ANDESITE_COUNTERTOP.get(),
+                    BlockRegistry.DARK_OAK_CABINET_WITH_POLISHED_ANDESITE_COUNTERTOP.get(),
+                    BlockRegistry.BAMBOO_CABINET_WITH_POLISHED_ANDESITE_COUNTERTOP.get(),
+                    BlockRegistry.CHERRY_CABINET_WITH_POLISHED_ANDESITE_COUNTERTOP.get(),
+                    BlockRegistry.MANGROVE_CABINET_WITH_POLISHED_ANDESITE_COUNTERTOP.get(),
+                    BlockRegistry.CRIMSON_CABINET_WITH_POLISHED_ANDESITE_COUNTERTOP.get(),
+                    BlockRegistry.WARPED_CABINET_WITH_POLISHED_ANDESITE_COUNTERTOP.get(),
+                    BlockRegistry.OAK_CABINET_WITH_POLISHED_TUFF_COUNTERTOP.get(),
+                    BlockRegistry.BIRCH_CABINET_WITH_POLISHED_TUFF_COUNTERTOP.get(),
+                    BlockRegistry.SPRUCE_CABINET_WITH_POLISHED_TUFF_COUNTERTOP.get(),
+                    BlockRegistry.JUNGLE_CABINET_WITH_POLISHED_TUFF_COUNTERTOP.get(),
+                    BlockRegistry.ACACIA_CABINET_WITH_POLISHED_TUFF_COUNTERTOP.get(),
+                    BlockRegistry.DARK_OAK_CABINET_WITH_POLISHED_TUFF_COUNTERTOP.get(),
+                    BlockRegistry.BAMBOO_CABINET_WITH_POLISHED_TUFF_COUNTERTOP.get(),
+                    BlockRegistry.CHERRY_CABINET_WITH_POLISHED_TUFF_COUNTERTOP.get(),
+                    BlockRegistry.MANGROVE_CABINET_WITH_POLISHED_TUFF_COUNTERTOP.get(),
+                    BlockRegistry.CRIMSON_CABINET_WITH_POLISHED_TUFF_COUNTERTOP.get(),
+                    BlockRegistry.WARPED_CABINET_WITH_POLISHED_TUFF_COUNTERTOP.get())));
+
+//    public static final RegistryObject<BlockEntityType<CabinetBlockEntity>> CABINET = TILES.register("cabinet",
+//            () -> new BlockEntityType<>(CabinetBlockEntity::new, Set.of(
+//                    BlockRegistry.PALE_OAK_CABINET.get())));
 }
